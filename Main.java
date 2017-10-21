830842eb2332d065a65b4f287d9fc37dbb48797c
LocyDragon <1115098044@qq.com>
2017-10-21 10:12:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.buttonlib.example;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.locydragon.lcv.api.ClassLevel;
+import com.locydragon.lcv.api.CodeList;
+import com.locydragon.lcv.api.NormalClassWriter;
+
+public class Main extends JavaPlugin{
+@Override
+public void onEnable() {
+	NormalClassWriter writer = new NormalClassWriter("Test");
+	CodeList list = new CodeList();
+	list.add("System.out.println(\"test\");");
+	writer.setStaticBlock(list);
+	CodeList list2 = new CodeList();
+	list2.add("System.out.println(what);");
+	writer.addMethod("test", list2, "String what", false, ClassLevel.PRIVATE, "void");
+	if (writer.write("C://test//class")) {
+		System.out.println("–¥»Î≥…π¶");
+	}
+}
+}
