31be17137e8619ecbe2e8d5b725def1b7f35ada8
LocyDragon <1115098044@qq.com>
2017-10-21 10:21:10 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,8 +14,8 @@ public void onEnable() {
 	list.add("System.out.println(\"test\");");
 	writer.setStaticBlock(list);
 	CodeList list2 = new CodeList();
-	list2.add("System.out.println(what);");
-	writer.addMethod("test", list2, "String what", false, ClassLevel.PRIVATE, "void");
+	list2.add("System.out.println(what+String.valueOf(i));");
+	writer.addMethod("test", list2, "String what, int i", false, ClassLevel.PRIVATE, "void");
 	if (writer.write("C://test//class")) {
 		System.out.println("写入代码成功");
 	}
